8a6dee1ff7101eff973518897c4676ff39765fb3
Victor Ibironke <82767685+vdiqbd@users.noreply.github.com>
2021-08-08 17:49:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -31,6 +31,40 @@ function showPosition() {
 	};
 };
 
+function doSimulatorA(div, pos) {
+	if (div < pos) {
+		let num = parseInt(storage.getItem('A-pos'));
+		num -= 1;
+		num.toString();
+		storage.setItem('A-pos', num);
+		showPosition();
+	} else {
+		let num = parseInt(storage.getItem('A-pos'));
+		num += 1;
+		num.toString();
+		storage.setItem('A-pos', num);
+		showPosition();
+	}
+	console.log(`A moved to floor ${pos}`);
+};
+
+function doSimulatorB(div, pos) {
+	if (div < pos) {
+		let num = parseInt(storage.getItem('B-pos'));
+		num -= 1;
+		num.toString();
+		storage.setItem('B-pos', num);
+		showPosition();
+	} else {
+		let num = parseInt(storage.getItem('B-pos'));
+		num += 1;
+		num.toString();
+		storage.setItem('B-pos', num);
+		showPosition();
+	}
+	console.log(`B moved to floor ${pos}`);
+};
+
 circleA[0].addEventListener('click', function() {
 	const interval = setInterval(function() {
 		if (circleA[0].innerText !== positionA.innerText) {
@@ -49,20 +83,7 @@ circleA[0].addEventListener('click', function() {
 circleA[1].addEventListener('click', function() {
 	const interval = setInterval(function() {
 		if (circleA[1].innerText !== positionA.innerText) {
-			if (circleA[1].innerText < positionA.innerText) {
-				let num = parseInt(storage.getItem('A-pos'));
-				num -= 1;
-				num.toString();
-				storage.setItem('A-pos', num);
-				showPosition();
-			} else {
-				let num = parseInt(storage.getItem('A-pos'));
-				num += 1;
-				num.toString();
-				storage.setItem('A-pos', num);
-				showPosition();
-			}
-			console.log(`A moved to floor ${positionA.innerText}`);
+			doSimulatorA(circleA[1].innerText, positionA.innerText);
 		} else {
 			clearInterval(interval);
 		}
@@ -72,20 +93,7 @@ circleA[1].addEventListener('click', function() {
 circleA[2].addEventListener('click', function() {
 	const interval = setInterval(function() {
 		if (circleA[2].innerText !== positionA.innerText) {
-			if (circleA[2].innerText < positionA.innerText) {
-				let num = parseInt(storage.getItem('A-pos'));
-				num -= 1;
-				num.toString();
-				storage.setItem('A-pos', num);
-				showPosition();
-			} else {
-				let num = parseInt(storage.getItem('A-pos'));
-				num += 1;
-				num.toString();
-				storage.setItem('A-pos', num);
-				showPosition();
-			}
-			console.log(`A moved to floor ${positionA.innerText}`);
+			doSimulatorA(circleA[2].innerText, positionA.innerText);
 		} else {
 			clearInterval(interval);
 		}
@@ -95,20 +103,7 @@ circleA[2].addEventListener('click', function() {
 circleA[3].addEventListener('click', function() {
 	const interval = setInterval(function() {
 		if (circleA[3].innerText !== positionA.innerText) {
-			if (circleA[3].innerText < positionA.innerText) {
-				let num = parseInt(storage.getItem('A-pos'));
-				num -= 1;
-				num.toString();
-				storage.setItem('A-pos', num);
-				showPosition();
-			} else {
-				let num = parseInt(storage.getItem('A-pos'));
-				num += 1;
-				num.toString();
-				storage.setItem('A-pos', num);
-				showPosition();
-			}
-			console.log(`A moved to floor ${positionA.innerText}`);
+			doSimulatorA(circleA[3].innerText, positionA.innerText);
 		} else {
 			clearInterval(interval);
 		}
@@ -118,20 +113,7 @@ circleA[3].addEventListener('click', function() {
 circleA[4].addEventListener('click', function() {
 	const interval = setInterval(function() {
 		if (circleA[4].innerText !== positionA.innerText) {
-			if (circleA[4].innerText < positionA.innerText) {
-				let num = parseInt(storage.getItem('A-pos'));
-				num -= 1;
-				num.toString();
-				storage.setItem('A-pos', num);
-				showPosition();
-			} else {
-				let num = parseInt(storage.getItem('A-pos'));
-				num += 1;
-				num.toString();
-				storage.setItem('A-pos', num);
-				showPosition();
-			}
-			console.log(`A moved to floor ${positionA.innerText}`);
+			doSimulatorA(circleA[4].innerText, positionA.innerText);
 		} else {
 			clearInterval(interval);
 		}
@@ -141,20 +123,7 @@ circleA[4].addEventListener('click', function() {
 circleA[5].addEventListener('click', function() {
 	const interval = setInterval(function() {
 		if (circleA[5].innerText !== positionA.innerText) {
-			if (circleA[5].innerText < positionA.innerText) {
-				let num = parseInt(storage.getItem('A-pos'));
-				num -= 1;
-				num.toString();
-				storage.setItem('A-pos', num);
-				showPosition();
-			} else {
-				let num = parseInt(storage.getItem('A-pos'));
-				num += 1;
-				num.toString();
-				storage.setItem('A-pos', num);
-				showPosition();
-			}
-			console.log(`A moved to floor ${positionA.innerText}`);
+			doSimulatorA(circleA[5].innerText, positionA.innerText);
 		} else {
 			clearInterval(interval);
 		}
@@ -164,20 +133,7 @@ circleA[5].addEventListener('click', function() {
 circleA[6].addEventListener('click', function() {
 	const interval = setInterval(function() {
 		if (circleA[6].innerText !== positionA.innerText) {
-			if (circleA[6].innerText < positionA.innerText) {
-				let num = parseInt(storage.getItem('A-pos'));
-				num -= 1;
-				num.toString();
-				storage.setItem('A-pos', num);
-				showPosition();
-			} else {
-				let num = parseInt(storage.getItem('A-pos'));
-				num += 1;
-				num.toString();
-				storage.setItem('A-pos', num);
-				showPosition();
-			}
-			console.log(`A moved to floor ${positionA.innerText}`);
+			doSimulatorA(circleA[6].innerText, positionA.innerText);
 		} else {
 			clearInterval(interval);
 		}
@@ -187,20 +143,7 @@ circleA[6].addEventListener('click', function() {
 circleA[7].addEventListener('click', function() {
 	const interval = setInterval(function() {
 		if (circleA[7].innerText !== positionA.innerText) {
-			if (circleA[7].innerText < positionA.innerText) {
-				let num = parseInt(storage.getItem('A-pos'));
-				num -= 1;
-				num.toString();
-				storage.setItem('A-pos', num);
-				showPosition();
-			} else {
-				let num = parseInt(storage.getItem('A-pos'));
-				num += 1;
-				num.toString();
-				storage.setItem('A-pos', num);
-				showPosition();
-			}
-			console.log(`A moved to floor ${positionA.innerText}`);
+			doSimulatorA(circleA[7].innerText, positionA.innerText);
 		} else {
 			clearInterval(interval);
 		}
@@ -210,20 +153,7 @@ circleA[7].addEventListener('click', function() {
 circleA[8].addEventListener('click', function() {
 	const interval = setInterval(function() {
 		if (circleA[8].innerText !== positionA.innerText) {
-			if (circleA[8].innerText < positionA.innerText) {
-				let num = parseInt(storage.getItem('A-pos'));
-				num -= 1;
-				num.toString();
-				storage.setItem('A-pos', num);
-				showPosition();
-			} else {
-				let num = parseInt(storage.getItem('A-pos'));
-				num += 1;
-				num.toString();
-				storage.setItem('A-pos', num);
-				showPosition();
-			}
-			console.log(`A moved to floor ${positionA.innerText}`);
+			doSimulatorA(circleA[8].innerText, positionA.innerText);
 		} else {
 			clearInterval(interval);
 		}
@@ -233,19 +163,11 @@ circleA[8].addEventListener('click', function() {
 circleA[9].addEventListener('click', function() {
 	const interval = setInterval(function() {
 		if (circleA[9].innerText !== positionA.innerText) {
-			if (circleA[9].innerText < positionA.innerText) {
-				let num = parseInt(storage.getItem('A-pos'));
-				num -= 1;
-				num.toString();
-				storage.setItem('A-pos', num);
-				showPosition();
-			} else {
-				let num = parseInt(storage.getItem('A-pos'));
-				num += 1;
-				num.toString();
-				storage.setItem('A-pos', num);
-				showPosition();
-			}
+			let num = parseInt(storage.getItem('A-pos'));
+			num += 1;
+			num.toString();
+			storage.setItem('A-pos', num);
+			showPosition();
 			console.log(`A moved to floor ${positionA.innerText}`);
 		} else {
 			clearInterval(interval);
@@ -271,20 +193,7 @@ circleB[0].addEventListener('click', function() {
 circleB[1].addEventListener('click', function() {
 	const interval = setInterval(function() {
 		if (circleB[1].innerText !== positionB.innerText) {
-			if (circleB[1].innerText < positionB.innerText) {
-				let num = parseInt(storage.getItem('B-pos'));
-				num -= 1;
-				num.toString();
-				storage.setItem('B-pos', num);
-				showPosition();
-			} else {
-				let num = parseInt(storage.getItem('B-pos'));
-				num += 1;
-				num.toString();
-				storage.setItem('B-pos', num);
-				showPosition();
-			}
-			console.log(`B moved to floor ${positionB.innerText}`);
+			doSimulatorB(circleB[1].innerText, positionB.innerText);
 		} else {
 			clearInterval(interval);
 		}
@@ -294,20 +203,7 @@ circleB[1].addEventListener('click', function() {
 circleB[2].addEventListener('click', function() {
 	const interval = setInterval(function() {
 		if (circleB[2].innerText !== positionB.innerText) {
-			if (circleB[2].innerText < positionB.innerText) {
-				let num = parseInt(storage.getItem('B-pos'));
-				num -= 1;
-				num.toString();
-				storage.setItem('B-pos', num);
-				showPosition();
-			} else {
-				let num = parseInt(storage.getItem('B-pos'));
-				num += 1;
-				num.toString();
-				storage.setItem('B-pos', num);
-				showPosition();
-			}
-			console.log(`B moved to floor ${positionB.innerText}`);
+			doSimulatorB(circleB[2].innerText, positionB.innerText);
 		} else {
 			clearInterval(interval);
 		}
@@ -317,20 +213,7 @@ circleB[2].addEventListener('click', function() {
 circleB[3].addEventListener('click', function() {
 	const interval = setInterval(function() {
 		if (circleB[3].innerText !== positionB.innerText) {
-			if (circleB[3].innerText < positionB.innerText) {
-				let num = parseInt(storage.getItem('B-pos'));
-				num -= 1;
-				num.toString();
-				storage.setItem('B-pos', num);
-				showPosition();
-			} else {
-				let num = parseInt(storage.getItem('B-pos'));
-				num += 1;
-				num.toString();
-				storage.setItem('B-pos', num);
-				showPosition();
-			}
-			console.log(`B moved to floor ${positionB.innerText}`);
+			doSimulatorB(circleB[3].innerText, positionB.innerText);
 		} else {
 			clearInterval(interval);
 		}
@@ -340,20 +223,7 @@ circleB[3].addEventListener('click', function() {
 circleB[4].addEventListener('click', function() {
 	const interval = setInterval(function() {
 		if (circleB[4].innerText !== positionB.innerText) {
-			if (circleB[4].innerText < positionB.innerText) {
-				let num = parseInt(storage.getItem('B-pos'));
-				num -= 1;
-				num.toString();
-				storage.setItem('B-pos', num);
-				showPosition();
-			} else {
-				let num = parseInt(storage.getItem('B-pos'));
-				num += 1;
-				num.toString();
-				storage.setItem('B-pos', num);
-				showPosition();
-			}
-			console.log(`B moved to floor ${positionB.innerText}`);
+			doSimulatorB(circleB[4].innerText, positionB.innerText);
 		} else {
 			clearInterval(interval);
 		}
@@ -363,20 +233,7 @@ circleB[4].addEventListener('click', function() {
 circleB[5].addEventListener('click', function() {
 	const interval = setInterval(function() {
 		if (circleB[5].innerText !== positionB.innerText) {
-			if (circleB[5].innerText < positionB.innerText) {
-				let num = parseInt(storage.getItem('B-pos'));
-				num -= 1;
-				num.toString();
-				storage.setItem('B-pos', num);
-				showPosition();
-			} else {
-				let num = parseInt(storage.getItem('B-pos'));
-				num += 1;
-				num.toString();
-				storage.setItem('B-pos', num);
-				showPosition();
-			}
-			console.log(`B moved to floor ${positionB.innerText}`);
+			doSimulatorB(circleB[5].innerText, positionB.innerText);
 		} else {
 			clearInterval(interval);
 		}
@@ -386,20 +243,7 @@ circleB[5].addEventListener('click', function() {
 circleB[6].addEventListener('click', function() {
 	const interval = setInterval(function() {
 		if (circleB[6].innerText !== positionB.innerText) {
-			if (circleB[6].innerText < positionB.innerText) {
-				let num = parseInt(storage.getItem('B-pos'));
-				num -= 1;
-				num.toString();
-				storage.setItem('B-pos', num);
-				showPosition();
-			} else {
-				let num = parseInt(storage.getItem('B-pos'));
-				num += 1;
-				num.toString();
-				storage.setItem('B-pos', num);
-				showPosition();
-			}
-			console.log(`B moved to floor ${positionB.innerText}`);
+			doSimulatorB(circleB[6].innerText, positionB.innerText);
 		} else {
 			clearInterval(interval);
 		}
@@ -409,20 +253,7 @@ circleB[6].addEventListener('click', function() {
 circleB[7].addEventListener('click', function() {
 	const interval = setInterval(function() {
 		if (circleB[7].innerText !== positionB.innerText) {
-			if (circleB[7].innerText < positionB.innerText) {
-				let num = parseInt(storage.getItem('B-pos'));
-				num -= 1;
-				num.toString();
-				storage.setItem('B-pos', num);
-				showPosition();
-			} else {
-				let num = parseInt(storage.getItem('B-pos'));
-				num += 1;
-				num.toString();
-				storage.setItem('B-pos', num);
-				showPosition();
-			}
-			console.log(`B moved to floor ${positionB.innerText}`);
+			doSimulatorB(circleB[7].innerText, positionB.innerText);
 		} else {
 			clearInterval(interval);
 		}
@@ -432,20 +263,7 @@ circleB[7].addEventListener('click', function() {
 circleB[8].addEventListener('click', function() {
 	const interval = setInterval(function() {
 		if (circleB[8].innerText !== positionB.innerText) {
-			if (circleB[8].innerText < positionB.innerText) {
-				let num = parseInt(storage.getItem('B-pos'));
-				num -= 1;
-				num.toString();
-				storage.setItem('B-pos', num);
-				showPosition();
-			} else {
-				let num = parseInt(storage.getItem('B-pos'));
-				num += 1;
-				num.toString();
-				storage.setItem('B-pos', num);
-				showPosition();
-			}
-			console.log(`B moved to floor ${positionB.innerText}`);
+			doSimulatorB(circleB[8].innerText, positionB.innerText);
 		} else {
 			clearInterval(interval);
 		}
@@ -455,19 +273,11 @@ circleB[8].addEventListener('click', function() {
 circleB[9].addEventListener('click', function() {
 	const interval = setInterval(function() {
 		if (circleB[9].innerText !== positionB.innerText) {
-			if (circleB[9].innerText < positionB.innerText) {
-				let num = parseInt(storage.getItem('B-pos'));
-				num -= 1;
-				num.toString();
-				storage.setItem('B-pos', num);
-				showPosition();
-			} else {
-				let num = parseInt(storage.getItem('B-pos'));
-				num += 1;
-				num.toString();
-				storage.setItem('B-pos', num);
-				showPosition();
-			}
+			let num = parseInt(storage.getItem('B-pos'));
+			num += 1;
+			num.toString();
+			storage.setItem('B-pos', num);
+			showPosition();
 			console.log(`B moved to floor ${positionB.innerText}`);
 		} else {
 			clearInterval(interval);
